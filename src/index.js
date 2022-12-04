2821b4ab2576d7dd6f620566cb9c1ca7872bf776
Patrick Jones <forksofpower@gmail.com>
2022-12-03 22:59:41 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hail Satan!</h1>
+<h1>Hello Trevor!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
